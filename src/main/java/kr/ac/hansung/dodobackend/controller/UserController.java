b907402cb96e8ddb8696b47d8399494cef29ef07
PSC <69509019+Prosted@users.noreply.github.com>
2023-05-16 13:59:00 +0900

    modify application.properties

--- src/main/java/kr/ac/hansung/dodobackend/controller/UserController.java
+++ src/main/java/kr/ac/hansung/dodobackend/controller/UserController.java
@@ -1,11 +1,11 @@
 package kr.ac.hansung.dodobackend.controller;
 
+import jakarta.validation.Valid;
 import kr.ac.hansung.dodobackend.model.User;
 import kr.ac.hansung.dodobackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,7 +24,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> AddUser(@Validated @RequestBody User user)
+    public ResponseEntity<?> AddUser(@Valid @RequestBody User user)
     {
         System.out.println("Entered user info : " + user);
         userService.AddUser(user);
