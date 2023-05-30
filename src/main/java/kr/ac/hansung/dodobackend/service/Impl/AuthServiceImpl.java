f5df26424401a6212765849c984f94ff8adac05e
postive12 <48992839+postive12@users.noreply.github.com>
2023-05-30 16:00:14 +0900

    Revert "Revert "Update authservice""
    This reverts commit 0f27136c6d6590e68272bebcb7109ad3bd69b5dc.

--- src/main/java/kr/ac/hansung/dodobackend/service/Impl/AuthServiceImpl.java
+++ src/main/java/kr/ac/hansung/dodobackend/service/Impl/AuthServiceImpl.java
@@ -19,50 +19,50 @@ import java.util.HashMap;
 
 @Component
 public class AuthServiceImpl implements AuthService {
-    private final String authToken;
-    private final String sid;
+//    private final String authToken;
+//    private final String sid;
     private Service service;
     private DefaultMessageService messageService;
     private HashMap<String,String> phoneCertMap = new HashMap<>();
 
     public AuthServiceImpl(@Value("${twilio.account_sid}") String sid, @Value("${twilio.auth_token}") String authToken, @Value("${twilio.service_id}") String serviceId) {
-        byte[] decodedBytes = Base64.getDecoder().decode(authToken);
-        String decodedString = new String(decodedBytes);
-        this.sid = sid;
-        this.authToken = decodedString;
-        Twilio.init(this.sid, this.authToken);
-        ResourceSet<Service> services = Service.reader().limit(20).read();
-        for(Service record : services) {
-            if (record.getSid().equals(serviceId)){
-                service = record;
-                return;
-            }
-        }
+//        byte[] decodedBytes = Base64.getDecoder().decode(authToken);
+//        String decodedString = new String(decodedBytes);
+//        this.sid = sid;
+//        this.authToken = decodedString;
+//        Twilio.init(this.sid, this.authToken);
+//        ResourceSet<Service> services = Service.reader().limit(20).read();
+//        for(Service record : services) {
+//            if (record.getSid().equals(serviceId)){
+//                service = record;
+//                return;
+//            }
+//        }
 //        //누리고 인증 테스트
-//        this.messageService = NurigoApp.INSTANCE.initialize("NCS6CFMZBL7DYBS8", "OL23WB98XKU1TDHBUXKGWML1XT8ZTRHP", "https://api.coolsms.co.kr");
+        this.messageService = NurigoApp.INSTANCE.initialize("NCS6CFMZBL7DYBS8", "OL23WB98XKU1TDHBUXKGWML1XT8ZTRHP", "https://api.coolsms.co.kr");
     }
     @Override
     public void sendVerification(String phoneNumber)
     {
-        Verification.creator(
-                        service.getSid(),
-                        "+82"+phoneNumber,
-                        "sms")
-                .create();
+//        Verification.creator(
+//                        service.getSid(),
+//                        "+82"+phoneNumber,
+//                        "sms")
+//                .create();
 //        //coolsms를 이용한 코드
-//        String certNum = createCertNum(6);
-//        Message message = new Message();
-//        message.setFrom("01049490367");
-//        message.setTo(phoneNumber);
-//        message.setText("인증번호는 " + certNum + "입니다");
-//        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-//        System.out.println(response);
-//        if (phoneCertMap.containsKey(phoneNumber)){
-//            phoneCertMap.replace(phoneNumber,certNum);
-//        }
-//        else {
-//            phoneCertMap.put(phoneNumber,certNum);
-//        }
+        String certNum = createCertNum(6);
+        Message message = new Message();
+        message.setFrom("01049490367");
+        message.setTo(phoneNumber);
+        message.setText("인증번호는 " + certNum + "입니다");
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println(response);
+        if (phoneCertMap.containsKey(phoneNumber)){
+            phoneCertMap.replace(phoneNumber,certNum);
+        }
+        else {
+            phoneCertMap.put(phoneNumber,certNum);
+        }
     }
 
     private String createCertNum(int length) {
@@ -76,17 +76,17 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public boolean checkVerification(String phoneNumber,String checkNum)
     {
-        var result = VerificationCheck.creator(service.getSid())
-                .setTo("+82"+phoneNumber)
-                .setCode(checkNum)
-                .create();
+//        var result = VerificationCheck.creator(service.getSid())
+//                .setTo("+82"+phoneNumber)
+//                .setCode(checkNum)
+//                .create();
 //        //coolsms를 이용한 코드
-//        if (!phoneCertMap.containsKey(phoneNumber)){
-//            return false;
-//        }
-//        String containedCert = phoneCertMap.get(phoneNumber);
-//        phoneCertMap.remove(phoneNumber);
+        if (!phoneCertMap.containsKey(phoneNumber)){
+            return false;
+        }
+        String containedCert = phoneCertMap.get(phoneNumber);
+        phoneCertMap.remove(phoneNumber);
 
-        return result.getStatus().equals("approved");
+        return containedCert.equals(checkNum);
     }
 }
