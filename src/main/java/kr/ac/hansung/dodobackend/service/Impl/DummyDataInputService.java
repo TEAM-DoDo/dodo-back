67f7a2af4df796c7bc191f0edd146b08f4203025
postive12 <postive12@gmail.com>
2023-06-01 01:44:31 +0900

    Update DummyDataInputService.java

--- src/main/java/kr/ac/hansung/dodobackend/service/Impl/DummyDataInputService.java
+++ src/main/java/kr/ac/hansung/dodobackend/service/Impl/DummyDataInputService.java
@@ -24,41 +24,98 @@ public class DummyDataInputService {
     public void createDummy(){
         //유저 더미데이터 생성 방법
         //첫번째 생성 친구
-        User user1 = 
+        User user201 =
                 User.builder()
                         .phoneNumber("010-1234-5678")
                         .category("[\"book\"\\,\"people-circle]")
                         .dateOfBirth("2000-01-01")
                         .gender("남").level(0)
-                        .nickname("테스트")
+                        .nickname("한수민")
                         .address("서울 노원구 동일로245길 162 은빛1단지아파트").build();
-        userRepository.save(user1);
+        userRepository.save(user201);
         //첫번째 생성 친구
-        User user2 =
+        User user202 =
                 User.builder()
                         .phoneNumber("010-1234-5678")
                         .category("[\"book\"\\,\"people-circle]")
                         .dateOfBirth("2002-01-01")
                         .gender("여").level(0)
-                        .nickname("테스트")
+                        .nickname("홍화영")
                         .address("서울 도봉구").build();
-        userRepository.save(user2);
+        userRepository.save(user202);
+        //첫번째 생성 친구
+        User user203 =
+                User.builder()
+                        .phoneNumber("010-2684-6248")
+                        .category("[\"book\"\\,\"people-circle]")
+                        .dateOfBirth("1998-02-01")
+                        .gender("남").level(0)
+                        .nickname("김군용")
+                        .address("서울 관악구 이곳 저곳").build();
+        userRepository.save(user203);
 
-        
+        //첫번째 생성 친구
+        User user204 =
+                User.builder()
+                        .phoneNumber("010-5876-3412")
+                        .category("[\"book\"\\,\"people-circle]")
+                        .dateOfBirth("1998-02-01")
+                        .gender("남").level(0)
+                        .nickname("김군용")
+                        .address("서울 관악구 이곳 저곳").build();
+        userRepository.save(user204);
+
+        //첫번째 생성 친구
+        User user205 =
+                User.builder()
+                        .phoneNumber("010-1324-4326")
+                        .category("[\"book\"\\,\"people-circle]")
+                        .dateOfBirth("1998-02-01")
+                        .gender("남").level(0)
+                        .nickname("김군용")
+                        .address("서울 관악구 이곳 저곳").build();
+        userRepository.save(user205);
+
+        //첫번째 생성 친구
+        User user206 =
+                User.builder()
+                        .phoneNumber("010-5313-1465")
+                        .category("[\"book\"\\,\"people-circle]")
+                        .dateOfBirth("2002-03-06")
+                        .gender("남").level(0)
+                        .nickname("하진수")
+                        .address("서울시 용산구 아이파크").build();
+        userRepository.save(user206);
+
         //Do 더미 생성
         //첫 번째 생성
-        Map<String,Object> result = new HashMap<>();
-        result.put("doName","테스트"); // 두 이름
-        result.put("place","테스트"); // 두 위치
-        result.put("description","테스트"); // 두 설명
-        result.put("userId",1);//생성한 유저의 고유 번호
-        doService.createNewDo(result);
+        Map<String,Object> do101 = new HashMap<>();
+        do101.put("doName","같이 여행다녀요"); // 두 이름
+        do101.put("place","서울시 관악구"); // 두 위치
+        do101.put("description","서울의 이곳저곳을 여행다니는 모임이에요 다들 친절하시니 같이 다녀요."); // 두 설명
+        do101.put("userId",1);//생성한 유저의 고유 번호
+        doService.createNewDo(do101);
+
+        Map<String,Object> do102 = new HashMap<>();
+        do102.put("doName","롯데백화점 수호대"); // 두 이름
+        do102.put("place","서울시 노원구"); // 두 위치
+        do102.put("description","노원구 롯데백화점에서 종종 하루 종일 돌아다니는 모임이에요."); // 두 설명
+        do102.put("userId",1);//생성한 유저의 고유 번호
+        doService.createNewDo(do102);
+
+        Map<String,Object> do103 = new HashMap<>();
+        do103.put("doName","같이 여행다녀요"); // 두 이름
+        do103.put("place","서울시 관악구"); // 두 위치
+        do103.put("description","서울의 이곳저곳을 여행다니는 모임이에요 다들 친절하시니 같이 다녀요."); // 두 설명
+        do103.put("userId",1);//생성한 유저의 고유 번호
+        doService.createNewDo(do103);
 
 //        //Do Schedule 더미 생성
 //        //첫 번째 생성
-//        ScheduleDTO scheduleDTO = new ScheduleDTO();
-//        scheduleDTO.setDoId(1L);//생성한 Do의 고유 번호
-//        scheduleDTO.setUserId(1L);//
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        scheduleDTO.setCost("10000");//두 비용
+        scheduleDTO.setDoId(1L);//생성한 Do의 고유 번호
+        scheduleDTO.setDoId(1L);//생성한 Do의 고유 번호
 
         //Do 사용자 참여 참여 생성하는 방법
         DoEnterDTO doEnterDTO = new DoEnterDTO();
